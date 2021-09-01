c602c5ba1e30a87b3c9d7aa41b88d7b8bb0ed16c
Shashwat <shashwatsanskar@gmail.com>
2021-09-01 21:03:07 +0530

    Complains and suggestions merged, user request page added, allot page updated

--- app.js
+++ app.js
@@ -6,11 +6,28 @@ const app = express();
 const path = require("path");
 const router = express.Router();
 const session = require("express-session");
-const expressValidator = require("express-validator");
-const message = require("express-messages");
 const customId = require("custom-id");
+const { request } = require("http");
+
+// var multer = require('multer');
+// var fs = require('fs');
+// var storage = multer.diskStorage({
+//   destination: './public/images/uploads/',
+//   filename: (req, file, cb) => {
+//     return cb(null, '$(file.fieldname)_${Date.now()}${path.extname(req.files["image"][0].filename)}')
+//   //   cb(null, file.fieldname + '-' + Date.now()
+//   // +  path.extname(file.originalname));
+//   }
+// });
+//
+// var upload = multer({
+//   storage: storage
+// });
 
 var guser = "";
+var u_username = "";
+var a_username = "";
+var idfordeletion = "";
 
 app.use(express.json());
 app.post('/user', (req, res) => {
@@ -19,8 +36,9 @@ app.post('/user', (req, res) => {
     password: req.body.password,
   }).then(user => res.json(user));
 });
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
 }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, 'node_modules')));
@@ -37,91 +55,123 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: true }
 }))
- //Express messages middleware
+//Express messages middleware
 
- app.use(require('connect-flash')());
+app.use(require('connect-flash')());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
   next();
 });
 
-mongoose.connect("mongodb://localhost:27017/HostelKeeperDB",{useNewUrlParser:true,
-                     useCreateIndex:true,
-                     useUnifiedTopology:true,
-                     useFindAndModify:false});
+mongoose.connect("mongodb://localhost:27017/HostelKeeperDB", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
 
 const requestschema = {
-  username:String,
-  date:String,
-  checkin:String,
-  checkout:String,
-  worktype:String
-
+  username: String,
+  date: String,
+  checkin: String,
+  checkout: String,
+  worktype: String,
 }
+
+//request model
+
 const Request = new mongoose.model("Request", requestschema);
 module.exports = mongoose.model('request', requestschema, 'request');
 
-const customerSchema = {
-  email:String,
-  contact:Number,
-  password:String,
-  name:String,
-  address:String,
-  request :Array
-};
-const Customer = new mongoose.model("Customer",customerSchema);
+//customer schema
 
+const customerSchema = {
+  email: String,
+  contact: Number,
+  password: String,
+  name: String,
+  address: String,
+  request: Array
+};
+
+//customer model
+
+const Customer = new mongoose.model("Customer", customerSchema);
+
+// admin schema
 
 const adminSchema = {
-  name:String,
-  email:String,
-  contact:Number,
-  city:String,
-  password:String
+  name: String,
+  email: String,
+  contact: Number,
+  city: String,
+  password: String
 };
-const Admin = new mongoose.model("Admin",adminSchema);
+
+//admin model
+
+const Admin = new mongoose.model("Admin", adminSchema);
+
+//hostelkeepers schema
+const hostelkeepersschema = {
+  name: String,
+  contact: Number,
+  address: String,
+  idtype: String,
+  hkrequests: Array
+};
+
+//hostelkeepers model
+
+const Hostelkeepers = new mongoose.model("Hostelkeepers", hostelkeepersschema);
 
 app.use(express.static("public"));
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
   res.render("home");
 })
 
-app.post("/register",function(req,res){
+app.post("/register", function (req, res) {
   const newCustomer = new Customer({
-    email:req.body.username,
-    contact:req.body.contact,
-    password:req.body.password,
-    name:req.body.name,
-    address:req.body.address,
-    request:[]
+    email: req.body.username,
+    contact: req.body.contact,
+    password: req.body.password,
+    name: req.body.name,
+    address: req.body.address,
+    // request:[]
   });
 
-  newCustomer.save(function(err){
-    if(err)
-    {
+
+  newCustomer.save(function (err) {
+    if (err) {
       console.log("Didn't saved");
-    }else {
-        Request.find({}, function(err, data) {
-      res.render("second", {requests:data});
+    } else {
+      console.log('user registered');
+      res.redirect("/");
+    }
   });
-  }
-});
 });
 
 
-app.post("/login", function(req,res){
-  const username = req.body.username;
+app.post("/login", function (req, res) {
+  u_username = req.body.username;
   const password = req.body.password;
-  Customer.findOne({email:username},function(err,foundCustomer){
-    if(err){
+  Customer.findOne({ email: u_username }, function (err, foundCustomer) {
+    if (err) {
       console.log(err);
-    }else{
-      if(foundCustomer){
-        if(foundCustomer.password === password){
-          Request.find({}, function(err, data) {
-        res.render("second", {requests:data});
-    });
+    } else {
+      if (foundCustomer) {
+        if (foundCustomer.password === password) {
+          Customer.findOne({email : u_username}, function (err, data) {
+            if (data !== null) {
+              console.log(data);
+              res.render("second", { requests: data.request, user: data });
+            }
+            else {
+              console.log("there is a error");
+            }
+          });
         }
       }
     }
@@ -129,8 +179,8 @@ app.post("/login", function(req,res){
 });
 
 
-app.post("/adminr",function(req,res){
-  console.log("Hello")
+app.post("/adminr", function (req, res) {
+  // console.log("Hello")
   const newAdmin = new Admin({
 
     name: req.body.name,
@@ -140,126 +190,211 @@ app.post("/adminr",function(req,res){
     password: req.body.password
   });
 
-  newAdmin.save(function(err){
-    if(err)
-    {
+  newAdmin.save(function (err) {
+    if (err) {
       console.log(err);
-    }else {
-      res.render("admin");
+    } else {
+      res.redirect("/");
+      // console.log('admin registered');
     }
   });
 });
 
-app.post("/adminl", function(req,res){
-  const username = req.body.username;
+app.post("/adminl", function (req, res) {
+  a_username = req.body.username;
   const password = req.body.password;
   //console.log(username,password);
-  Admin.findOne({email:username},function(err,foundAdmin){
-    if(err){
+  Admin.findOne({ email: a_username }, function (err, foundAdmin) {
+    if (err) {
       console.log(err);
-    }else{
-      if(foundAdmin){
-        if(foundAdmin.password === password){
-          res.render("admin");
+    } else {
+      if (foundAdmin) {
+        if (foundAdmin.password === password) {
+          Request.find({}, function (err, data) {
+            res.render("admin", { requests: data });
+          });
+        }
+        else{
+          console.log(err);
         }
       }
+      else{
+        console.log("admin not found!")
+      }
     }
   });
 });
 
-app.post("/requestsinput", function(req,res){
+app.post("/requestsinput", function (req, res) {
 
-   // console.log(req.body);
+  // console.log(req.body);
   const newrequest = new Request({
-    username:req.body.username,
-    date:req.body.date,
-    checkin:req.body.checkin,
-    checkout:req.body.checkout,
-    worktype:req.body.worktype
+    username: req.body.username,
+    date: req.body.date,
+    checkin: req.body.checkin,
+    checkout: req.body.checkout,
+    worktype: req.body.worktype
   });
   guser = req.body.username;
-  Customer.findOne({email:guser},function(err,user){
+  Customer.findOne({ email: guser }, function (err, user) {
     user.request.push(newrequest);
-    console.log("req added->"+newrequest);
+    // console.log("req added->" + newrequest);
+    // idfordeletion = newrequest._id;
     user.save();
-  })
- // var errors = req.validationErrors();
-
- // if(errors)
- //   console.log("errors");
- newrequest.save(function(err){
-   if(err)
-   {
-     console.log("Didn't saved");
-   }else {
-     res.redirect("profile")
-      }
- });
-
-
-});
-
-app.get("/admindashboard",function(req,res){
-  res.render("admin");
-});
-
-app.get("/allot",function(req,res){
-  res.render("allot");
-});
-
-app.get("/complaints",function(req,res){
-  res.render("complaints");
-});
-
-app.get("/suggestions", (req, res) => {
-    res.render('suggestions');
-   });
-
-app.get("/registerstudents",function(req,res){
-  Request.find({}, function(err, data) {
-res.render("second", {requests:data});
-});
-});
-app.get("/registerhostelkeeper",function(req,res){
-  res.render("registerhostelkeeper");
-});
-
-app.get("/requests",function(req,res){
-  Request.find({}, function(err, data) {
-res.render("requests", {requests:data});
-});
-});
-app.get("/userdashboard",function(req,res){
-  Request.find({}, function(err, data) {
-res.render("second", {requests:data});
-});
-
-})
-app.get('/feedback', (req, res) => {
-  Request.find({}, function(err, data) {
-res.render("feedback", {requests:data});
-});
-   });
-
-app.get("/profile",function(req,res){
- const username = guser;
-  console.log("guser-> "+guser);
-  Customer.findOne({email:username}, function(err, data) {
-    if (data!==null) {
-console.log(data);
-      res.render("profile", {requests:data.request,user:data});
+  });
+  // Hostelkeepers.find({},function(err,hk){
+  //   hk.hkrequests.push(newrequest);
+  //   hk.save();
+  // });
+  newrequest.save(function (err) {
+    if (err) {
+      console.log("Didn't saved");
+    } else {
+      res.redirect("profile");
     }
-   });
   });
 
-app.get("/Logout",function(req,res){
-  res.render("home");
 });
-app.get("/second",function(req,res){
-  Request.find({}, function(err, data) {
-res.render("second", {requests:data});
+
+app.post("/hkID", function(req,res){
+
+  const newhostelkeeper = new Hostelkeepers({
+    name: req.body.name,
+    contact: req.body.contact,
+    address: req.body.address,
+    idtype: req.body.idtype,
+  });
+  // Hostelkeepers.find({},function(err,hk){
+  //   hk.hkrequests.push(newrequest);
+  //   hk.save();
+  // });
+  newhostelkeeper.save(function (err) {
+    if (err) {
+      console.log("Didn't saved");
+    } else {
+      res.redirect("allot");
+    }
+  });
 });
+
+
+
+app.get("/admindashboard", function (req, res) {
+  Request.find({}, function (err, data) {
+    res.render("admin", { requests: data });
+  });
 });
-app.listen(3000,function(){
-    console.log("Server started on port 3000");
+
+app.get("/allot", function (req, res) {
+ Hostelkeepers.find({}, function (err, data) {
+    res.render("allot", { workers: data });
+  });
+});
+
+app.get("/complaints", function (req, res) {
+  Request.find({}, function (err, data) {
+    res.render("complaints", { requests: data });
+  });
+});
+
+
+app.get("/registerstudents", function (req, res) {
+  Request.find({}, function (err, data) {
+    res.render("registerstudents", { requests: data });
+  });
+});
+app.get("/registerhostelkeeper", function (req, res) {
+  Request.find({}, function (err, data) {
+    res.render("registerhostelkeeper", { requests: data });
+  });
+});
+
+app.get("/requests", function (req, res) {
+
+  Customer.findOne({email : u_username}, function (err, data) {
+    if (data !== null) {
+      // console.log(data);
+      res.render("requests", { requests: data.request, user: data });
+    }
+    else {
+      console.log("there is a error");
+    }
+  });
+
+});
+
+app.get("/userrequests", function(req,res){
+
+  Request.find({}, function(err,data){
+    if (data !== null) {
+      res.render("userrequests", { requests: data });
+    }
+    else {
+      console.log("there is a error");
+    }
+  });
+});
+
+app.get("/userdashboard", function (req, res) {
+  const emailtobeused = u_username;
+  Customer.findOne({email : emailtobeused}, function (err, data) {
+    if (data !== null) {
+      // console.log(data);
+      res.render("second", { requests: data.request, user: data });
+    }
+    else {
+      console.log("there is a error");
+    }
+  });
+
+  });
+
+app.get('/feedback', (req, res) => {
+  Customer.findOne({email : u_username}, function (err, data) {
+    if (data !== null) {
+      // console.log(data);
+      res.render("feedback", { requests: data.request, user: data });
+    }
+    else {
+      console.log("there is a error");
+    }
+  });
+});
+
+app.post("/deletereq", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+
+  Customer.findByIdAndRemove(checkedItemId, function (err) {
+    if (!err) {
+      console.log("Successfully deleted item.");
+      res.redirect("/profile");
+    }
+  });
+});
+
+
+app.get("/profile", function (req, res) {
+  const usernameprof = u_username;
+  Customer.findOne({ email: usernameprof }, function (err, data) {
+    if (data !== null) {
+      // console.log(data);
+      res.render("profile", { requests: data.request, user: data });
+    }
+    else {
+      console.log("there is a error");
+    }
+  });
+});
+
+
+app.get("/Logout", function (req, res) {
+  res.redirect("/");
+});
+// app.get("/second", function (req, res) {
+//   Customer.find({}, function (err, data) {
+//     res.render("second", { requests: data.request });
+//   });
+// });
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
